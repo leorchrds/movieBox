f43f07afdefc7c037ad138d5d5ef35bc7f2a4d02
leorchrd <leoonardorichard@gmail.com>
2025-02-06 11:45:32 -0300

    adjusting the association between User and Movie, adding a new field for poster and updating routes.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,6 +9,9 @@ class Movie
   field :director, type: String
   field :synopsis, type: String
   field :user_comment, type: String
+  field :poster, type: String
 
   validates :imdb_id, presence: true, uniqueness: true
+
+  has_and_belongs_to_many :favorited_by_users, class_name: 'User'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,23 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root "movies#index"
-  
-   # pesquisa de filmes
-   get 'movies/search', to: 'movies#search', as: 'search_movies'
+  root 'movies#index'
 
-   resources :movies, only: [:index, :show, :new, :create, :edit]
+  # pesquisa de filmes
+  get 'movies/search', to: 'movies#search', as: 'search_movies'
+
+  resources :movies, only: %i[index show new create edit update destroy] do
+    collection do
+      post 'favorite'
+    end
+
+    collection do
+      get 'favorites'
+    end
+  end
 end
